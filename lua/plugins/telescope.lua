8e4535816c214a0279dfe5d18a0dbd666393eda6
Marcos Venicius <marcosdevelopment.business@mail.com>
2024-07-12 10:36:26 -0300

    feat: grep current file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -32,6 +32,9 @@ return {
       pcall(require('telescope').load_extension, 'fzf')
       vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
       vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>t', function ()
+        require("telescope.builtin").live_grep({search_dirs={vim.fn.expand("%:p")}})
+      end, { desc = 'Grep current file' })
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to telescope to change theme, layout, etc.
         require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
