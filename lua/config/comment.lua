9845c49068abb61e13b7b827135d1410e0f3cf3f
Marcos Venicius <marcosdevelopment.business@gmail.com>
2024-08-20 08:03:31 -0300

    add comment plugin

---
+++ lua/config/comment.lua
@@ -0,0 +1,10 @@
+require('Comment').setup {
+  toggler = {
+    line = '<C-_>',
+    block = '<C-_>',
+  },
+  opleader = {
+    line = '<C-_>',
+    block = '<C-_>',
+  }
+}
---
+++ lua/plugins/comment.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('config.comment')
+    end
+  }
+}
